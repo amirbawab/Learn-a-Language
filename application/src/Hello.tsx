37df5f5f4afb20fbf77aae2eaf8f3cfee7a98f9e
Amir Bawab <amirbawab@gmail.com>
2019-11-03 17:30:12 -0500

    Init application

---
+++ application/src/Hello.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Hello extends React.Component {
+    state = { }
+    render() { 
+        return ( 
+            <h1>Hello world!!</h1>
+         );
+    }
+}
+ 
+export default Hello;
