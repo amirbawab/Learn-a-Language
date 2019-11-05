25e12a0ff607529b593ebe27f1a496a415902c54
Amir Bawab <amirbawab@gmail.com>
2019-11-05 17:40:11 -0500

    Added example form

--- application/src/pages/sections/Example.tsx
+++ application/src/pages/sections/Example.tsx
@@ -7,36 +7,64 @@ export interface LLExampleProps {
 }
 export interface LLExampleState {}
 class LLExample extends React.Component<LLExampleProps, LLExampleState> {
+  state = {
+    form_hidden: true,
+  }
+  set_form_hidden(e : any, is_hidden : boolean) {
+    e.preventDefault();
+    this.setState({'form_hidden': is_hidden});
+  }
   render() {
-    return (
-      <div className="row">
-        <div className="col-lg-12">
-          <LLBasicCard title="Examples">
-            {this.props.data.map((example, example_id) => {
-              return (
-                <div key={example_id}>
-                  <div>{example.get_sentence()}</div>
-                  <div>{example.get_sounds().map((sound, sound_id) => {
-                    return (
-                      <div key={sound_id}>
-                        <span className="text-primary mr-2">[{sound.get_language()} Pronunciation]</span>
-                        <span>{sound.get_sound()}</span>
-                      </div>
-                    );
-                  })}</div>
-                  <a href="#"><i className="fas fa-plus-square"></i> Add Pronunciation</a>
-                  <div className="border-top my-3"></div>
-                </div>
-              );
-            })}
-            <a href="#" className="btn btn-primary btn-icon-split">
-              <span className="icon text-white-50">
-                <i className="fas fa-plus-square"></i>
-              </span>
-              <span className="text">Add Example</span>
-            </a>
-          </LLBasicCard>
+    let form = undefined;
+    if(!this.state.form_hidden) {
+      form = (
+        <div className="row">
+          <div className="col-lg-12">
+            <LLBasicCard>
+              <div className="form-group">
+                <label>Sentence</label>
+                <input type="text" className="form-control" id="native"/>
+              </div>
+              <button className="btn btn-primary mr-2">Save</button>
+              <button className="btn btn-secondary" onClick={(e) => this.set_form_hidden(e, true)}>Close</button>
+            </LLBasicCard>
+          </div>
         </div>
+      );
+    }
+
+    return (
+      <div>
+        <div className="row">
+          <div className="col-lg-12">
+            <LLBasicCard title="Examples">
+              {this.props.data.map((example, example_id) => {
+                return (
+                  <div key={example_id}>
+                    <div>{example.get_sentence()}</div>
+                    <div>{example.get_sounds().map((sound, sound_id) => {
+                      return (
+                        <div key={sound_id}>
+                          <span className="text-primary mr-2">[{sound.get_language()} Pronunciation]</span>
+                          <span>{sound.get_sound()}</span>
+                        </div>
+                      );
+                    })}</div>
+                    <a href="#"><i className="fas fa-plus-square"></i> Add Pronunciation</a>
+                    <div className="border-top my-3"></div>
+                  </div>
+                );
+              })}
+              <a href="#" onClick={(e) => this.set_form_hidden(e, false)} className="btn btn-primary btn-icon-split">
+                <span className="icon text-white-50">
+                  <i className="fas fa-plus-square"></i>
+                </span>
+                <span className="text">Add Example</span>
+              </a>
+            </LLBasicCard>
+          </div>
+        </div>
+        {form}
       </div>
     );
   }
