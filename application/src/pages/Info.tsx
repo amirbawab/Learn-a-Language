c74e882eb481cb257df2467e03584a1dd0b9f270
Amir Bawab <amirbawab@gmail.com>
2019-11-21 17:54:45 -0500

    Updated welcome page

--- application/src/pages/Info.tsx
+++ application/src/pages/Info.tsx
@@ -15,7 +15,52 @@ class LLInfo extends React.Component<LLInfoProps, LLInfoState> {
         <div className="row">
           <div className="col-lg-12">
             <LLBasicCard>
-              <h5>Click on "New Word" or select an existing one</h5>
+              <b>How to use this app?</b>
+              <p>Start by cloning and installing dependencies:</p>
+              <code><pre>{`
+git clone https://github.com/amirbawab/learn-a-language
+cd learn-a-language/
+npm install
+npm run application
+`}
+              </pre></code>
+              <hr/>
+              <b>How to add a new Word?</b>
+              <p>Create a new JSON file and place it inside the <code>data/</code> directory located at the root of this project. 
+                 <u>The page should automatically update.</u></p>
+              <code><pre>
+              {`
+{
+  "word":"Tea",
+  "alias":"tea",
+  "native_form":["茶"],
+  "example":[
+    {
+      "sentence":"I want tea",
+      "pronunciation":[
+        {
+          "language":"English",
+          "sound":"wo yao chai"
+        }
+      ]
+    }
+  ],
+  "pronunciation":[
+    {
+      "language":"English",
+      "sound":"Chai"
+    }
+  ]
+}
+                `}
+              </pre></code>
+
+              <hr/>
+              <b>How to reference another word?</b>
+              <p>
+                In the <code>native_form</code> JSON property, use <code>#</code> followed by the alias of
+                the word you would like to reference. (e.g. <code>#tea</code>)
+              </p>
             </LLBasicCard>
           </div>
         </div>
