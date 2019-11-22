43dcc4e5dd3e88fe9be78436f91c1a6ea756b1eb
Amir Bawab <amirbawab@gmail.com>
2019-11-22 00:05:51 -0500

    Update repo

--- README.md
+++ README.md
@@ -4,11 +4,4 @@
 [Demo Link](https://amirbawab.github.io/Learn-a-Language/)  
 
 ## Usage
-- Fork repository then clone
-- Open a new terminal
-```
-cd learn-a-language/
-npm install
-npm run application
-```
-- Create a new word in the data file. More information in the demo link above.
+Instruction available in the demo
--- application/src/pages/Info.tsx
+++ application/src/pages/Info.tsx
@@ -15,15 +15,35 @@ class LLInfo extends React.Component<LLInfoProps, LLInfoState> {
         <div className="row">
           <div className="col-lg-12">
             <LLBasicCard>
-              <b>How to add a new Word?</b>
-              <p>Create a new JSON file and place it inside the <code>data/</code> directory located at the root of this project. 
-                 <u>The page should automatically update.</u></p>
-              <code><pre>
-              {`
-{
+              <section>
+                <h4>How to use this application</h4>
+                <div>
+                  <ul>
+                    <li>Fork the <a href="https://github.com/amirbawab/Learn-a-Language">repository</a></li>
+                    <li>Clone your forked version: <code>git clone https://github.com/YOUR_USERNAME/Learn-a-Language</code></li>
+                    <li>In a new terminal type: <code>cd Learn-a-Language/application/ && npm install && npm run application</code></li>
+                    <li>Open browser at <code>http://localhost:3000/</code> (or select the corresponding port if 3000 was occupied)</li>
+                    <li><a href="#add-word">Add a new word</a> by creating a JSON file inside <code>Learn-a-Language/application/data/</code> directory</li>
+                    <li>Upon creating or saving a JSON file, the application should automatically refresh</li>
+                    <li>Add, commit and push your JSON files to your Github repository</li>
+                  </ul>
+                </div>
+              </section>
+              <hr/>
+              <section id="add-word">
+                <h4>Word JSON file</h4>
+                <p>Example of a word JSON file</p>
+                <code style={{display: "block", whiteSpace: "pre-wrap"}}>
+              {`{
   "word":"Tea",
   "alias":"tea",
   "native_form":["茶"],
+  "pronunciation":[
+    {
+      "language":"English",
+      "sound":"Chai"
+    }
+  ],
   "example":[
     {
       "sentence":"I want tea",
@@ -34,23 +54,18 @@ class LLInfo extends React.Component<LLInfoProps, LLInfoState> {
         }
       ]
     }
-  ],
-  "pronunciation":[
-    {
-      "language":"English",
-      "sound":"Chai"
-    }
   ]
-}
-                `}
-              </pre></code>
-
+}`}
+                </code>
+              </section>
               <hr/>
-              <b>How to reference another word?</b>
-              <p>
-                In the <code>native_form</code> JSON property, use <code>#</code> followed by the alias of
-                the word you would like to reference. (e.g. <code>#tea</code>)
-              </p>
+              <section>
+                <h4>How to reference another word?</h4>
+                <p>
+                  In the <code>native_form</code> JSON property, use <code>#</code> followed by the alias of
+                  the word you would like to reference. (e.g. <code>#tea</code>)
+                </p>
+              </section>
             </LLBasicCard>
           </div>
         </div>
