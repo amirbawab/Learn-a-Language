d3b06b1539adf8dfbe4306c8e6d09fc06e4d959f
Amir Bawab <amirbawab@gmail.com>
2019-11-22 00:49:14 -0500

    Updated info

--- application/src/pages/Info.tsx
+++ application/src/pages/Info.tsx
@@ -19,13 +19,15 @@ class LLInfo extends React.Component<LLInfoProps, LLInfoState> {
                 <h4>How to use this application</h4>
                 <div>
                   <ul>
-                    <li>Fork the <a href="https://github.com/amirbawab/Learn-a-Language">repository</a></li>
-                    <li>Clone your forked version: <code>git clone https://github.com/YOUR_USERNAME/Learn-a-Language</code></li>
-                    <li>In a new terminal type: <code>cd Learn-a-Language/application/ && npm install && npm run application</code></li>
-                    <li>Open browser at <code>http://localhost:3000/</code> (or select the corresponding port if 3000 was occupied)</li>
-                    <li><a href="#add-word">Add a new word</a> by creating a JSON file inside <code>Learn-a-Language/application/data/</code> directory</li>
-                    <li>Upon creating or saving a JSON file, the application should automatically refresh</li>
-                    <li>Add, commit and push your JSON files to your Github repository</li>
+                    <li>Create a new repository: <code>my-language</code></li>
+                    <li>Clone your repository: <code>git clone https://github.com/USERNAME/my-language.git</code></li>
+                    <li>Open a new terminal and type: <br/> <code>cd my-language/ && git submodule add https://github.com/amirbawab/Learn-a-Language.git</code></li>
+                    <li>Create a diretory to store your words JSON files: <code>mkdir data</code></li>
+                    <li>Install packages: <code>npm install --prefix ./Learn-a-Language/application/</code></li>
+                    <li>Start application: <code>LL_DATA_DIR="$PWD/data/" npm run application --prefix ./Learn-a-Language/application/</code></li>
+                    <li>Open your browser at <a href="http://localhost:3000/">http://localhost:3000/</a></li>
+                    <li><a href="#add-word">Add new words</a> into the <code>data</code> directory</li>
+                    <li>Upon create or save, the application should automatically refresh</li>
                   </ul>
                 </div>
               </section>
@@ -61,10 +63,18 @@ class LLInfo extends React.Component<LLInfoProps, LLInfoState> {
               <hr/>
               <section>
                 <h4>How to reference another word?</h4>
-                <p>
+                <div>
                   In the <code>native_form</code> JSON property, use <code>#</code> followed by the alias of
                   the word you would like to reference. (e.g. <code>#tea</code>)
-                </p>
+                </div>
+              </section>
+              <section>
+                <h4>Publish to Github pages</h4>
+                <div>
+                  <ul>
+                    <li>Create github worflows directory: <code>mkdir -p .github/workflows/</code></li>
+                  </ul>
+                </div>
               </section>
             </LLBasicCard>
           </div>
