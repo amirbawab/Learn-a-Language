f49649508103f94a4e070d51fa3d52dee4e126dd
amirbawab <amirbawab@gmail.com>
2020-01-12 05:32:00 +0000

    Deploying to gh-pages from master 0baf2eade40788dc2964916d43155a5e358ad291

---
+++ precache-manifest.279dc3bbda410651dfa9234b9bf298aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f4fefeb3a78ce5609ea5ef616014ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5b9d2ab00b2de57a555",
+    "url": "./static/js/2.fc6b1442.chunk.js"
+  },
+  {
+    "revision": "c09d301a6e352c3197a4",
+    "url": "./static/js/main.12d3ecd9.chunk.js"
+  },
+  {
+    "revision": "acef1e8350518edcd4ac",
+    "url": "./static/js/runtime-main.ab0b36f6.js"
+  }
+]);
